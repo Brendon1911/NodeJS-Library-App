8bced5f909f9a759d4a3fa59b363ed34c676c9b7
Brendon Richards <brendonrichards93@gmail.com>
2018-08-21 05:03:44 +0000

    Update app.js to serve files from node modules

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ app.use(morgan("tiny"));
 // Use public files
 app.use(express.static(path.join(__dirname, "/public")));
 
+// Use Bootstrap CSS
+app.use("/css", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css")));
+
+// Use Bootstrap JS
+app.use("/js", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js")));
+
+// Use jQuery
+app.use("/js", express.static(path.join(__dirname, "/node_modules/jquery/dist")));
+
+// Use Popper.js
+app.use("/js", express.static(path.join(__dirname, "/node_modules/popper.js/dist/umd")));
+
 // Home route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"));
