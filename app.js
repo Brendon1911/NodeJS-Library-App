a5906691f891a490cb85b76fbe6c38922248791d
Brendon Richards <brendonrichards93@gmail.com>
2018-08-22 10:40:55 +0000

    Implement routing & books view

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const port = process.env.PORT || 3000;
 // Declare app
 const app = express();
 
+// Routing
+const bookRouter = express.Router();
+
 // Use Morgan
 app.use(morgan('tiny'));
 
@@ -33,6 +36,56 @@ app.set('views', './src/views');
 
 // Set Pug templating engine
 app.set('view engine', 'ejs');
+
+const books = [
+  {
+    title: 'The Subway Girls',
+    genre: 'Historical Fiction',
+    author: ' Susie Orman Schnall',
+    read: false
+  },
+  {
+    title: 'War',
+    genre: 'Fantasy',
+    author: 'Jennifer Anne Davis',
+    read: false
+  },
+  {
+    title: 'Sanctuary',
+    genre: 'Science Fiction',
+    author: 'Caryn Lix',
+    read: false
+  },
+  {
+    title: '1984',
+    genre: 'Science Fiction',
+    author: 'George Orwell',
+    read: false
+  },
+  {
+    title: 'Of Mice and Men',
+    genre: 'Historical Fiction',
+    author: 'John Steinbeck',
+    read: false
+  }
+];
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render('books', {
+      nav: [{ link: '/books', title: 'books' },
+            { link: '/authors', title: 'authors' }],
+      title: 'Node Library App',
+      books
+    });
+  });
+  
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('Hello books');
+  });
+
+app.use('/books', bookRouter);
 
 // Index route
 app.get('/', (req, res) => {
