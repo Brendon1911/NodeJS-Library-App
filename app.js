eefdf21717534b2ea63c4ba646e291f25ad2847e
Brendon Richards <brendonrichards93@gmail.com>
2018-08-23 11:36:53 +0000

    Connect to and use database

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
+// Set port
 const port = process.env.PORT || 3000;
 
 // Declare app
@@ -14,8 +16,22 @@ const app = express();
 const nav = [{ link: '/books', title: 'books' },
              { link: '/authors', title: 'authors' }];
 
-// Routing
-const bookRouter = require("./src/routes/bookRoutes")(nav);
+// Require routes
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+
+// Configure Azure database
+const config = {
+    user: 'library',
+    password: '$cuderiaF40',
+    server: 'node-library.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+    database: 'Node-Library-App',
+ 
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
+};
+
+sql.connect(config).catch(err => debug(err));
 
 // Use Morgan
 app.use(morgan('tiny'));
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,6 +1,10 @@
-const express = require("express");
+const express = require('express');
 
 const bookRouter = express.Router();
+
+const sql =require("mssql");
+
+const debug = require('debug')('app:bookRoutes')
 
 function router (nav) {
   const books = [
@@ -38,25 +42,37 @@ function router (nav) {
   
   bookRouter.route('/')
     .get((req, res) => {
-      res.render('bookListView', 
-      {
-        nav,
-        title: 'Node Library App',
-        books
-      });
+      (async function query(){
+        const request = new sql.Request();
+      
+      const { recordset } = await request.query('select * from books')
+        .then((result) => {
+          res.render('bookListView', 
+          {
+            nav,
+            title: 'Node Library App',
+            books: recordset
+          });
+        });
+      }());
     });
     
   bookRouter.route('/:id')
     .get((req, res) => {
-      const { id } = req.params;
-      res.render('bookView', 
-      {
-        nav,
-        title: 'Node Library App',
-        book: books[id]
-      });
+      (async function query (){
+        const { id } = req.params;
+        const request = new sql.Request();
+        const { recordset } = 
+          await request.input('id', sql.Int, id)
+            .query('select * from books where id = @id');
+        res.render('bookView', 
+        {
+          nav,
+          title: 'Node Library App',
+          book: recordset[0]
+        });
+      }());
     });
-    
   return bookRouter;
 }
   
